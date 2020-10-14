585cd92444dbd1372e616bae9963db3283624e65
ZungryWare <calvinmartinezdv@gmail.com>
2020-10-14 15:37:25 -0700

    Added base code for activity requests. (#36)
    * Added base code for activity requests.
    Closes #27 and closes #31

--- backend/api/activity/index.js
+++ backend/api/activity/index.js
@@ -3,14 +3,43 @@ const express = require('express');
 const activity = express.Router();
 
 /**
- * Finds posts by id
- * @param {Number} id - The id of the post, must be only digits
+ * Returns activity data by id
+ * @param {Number} id - The id of the activity, must be only digits
  */
 activity.route('/:id([0-9]+)')
   .get((req, res) => {
     const statusCode = 200;
+	const shortened = Boolean(req.query.shortened);
     res.status(statusCode).json({
       id: req.params.id,
+      status: statusCode,
+	  shortened,
+    });
+  });
+
+/**
+ * Edits activity by id
+ * @param {Number} id - The id of the activity, must be only digits
+ */
+activity.route('/edit/:id([0-9]+)')
+  .post((req, res) => {
+    const statusCode = 200;
+    res.status(statusCode).json({
+      id: req.params.id,
+      status: statusCode,
+    });
+  });
+
+/**
+ * Creates a new activity and returns its id
+ * @param {Number} id - The id of the activity, must be only digits
+ */
+activity.route('/create')
+  .post((req, res) => {
+    const statusCode = 200;
+	//res.redirect('/activity/edit/#');
+    res.status(statusCode).json({
+      id: 666,
       status: statusCode,
     });
   });
