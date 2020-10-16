6820ce6a8a4262ded1ceaf9a7f08e33d784513d2
ZungrySoft <calvinmartinezdv@gmail.com>
2020-10-15 21:07:47 -0700

    Added search request. Connected create, search, and get requests to database.

--- backend/api/activity/index.js
+++ backend/api/activity/index.js
@@ -1,47 +1,73 @@
 const express = require('express');
+const Activity = require('../../database/models/activity');
 
 const activity = express.Router();
 
 /**
- * Returns activity data by id
- * @param {Number} id - The id of the activity, must be only digits
+ * Creates a new activity and returns its data
+ * @body {String} title - The title of the post
+ * @body {String} description - The title of the post
  */
-activity.route('/:id([0-9]+)')
-  .get((req, res) => {
+activity.route('/create')
+  .post(async(req, res) => {
     const statusCode = 200;
-	const shortened = Boolean(req.query.shortened);
-    res.status(statusCode).json({
-      id: req.params.id,
-      status: statusCode,
-	  shortened,
-    });
+	const currentDate = Date.now();
+	const activity = new Activity({
+		title: req.body.title,
+		description: req.body.description,
+		creationDateTime: currentDate,
+		lastEditDateTime: currentDate,
+	})
+	await activity.save()
+	//res.redirect('/activity/edit/#');
+    res.status(statusCode).send(activity);
   });
-
+  
 /**
  * Edits activity by id
+ * TODO: Implement this
  * @param {Number} id - The id of the activity, must be only digits
  */
 activity.route('/edit/:id([0-9]+)')
-  .post((req, res) => {
+  .post(async(req, res) => {
     const statusCode = 200;
     res.status(statusCode).json({
       id: req.params.id,
       status: statusCode,
     });
   });
+  
+/**
+ * Searches posts by criteria specified in post data
+ * TODO: Add search criteria
+ * @body {Number} numPosts - The number of posts to return
+ */
+activity.route('/search')
+  .get(async(req, res) => {
+	numPosts = 20;
+	if (Boolean(req.body.numPosts)) {
+		numPosts = req.body.numPosts;
+	}
+	let getobj = await Activity.find( {creationDateTime: {$exists: true}} ).sort({creationDateTime : -1}).limit(numPosts);
+	
+    const statusCode = 200;
+	
+    res.status(statusCode).send(getobj);
+  });
 
 /**
- * Creates a new activity and returns its id
+ * Returns activity data by id
+ * Note that this route is on bottom to prevent any above routes from matching to this by only having letters a-f
  * @param {Number} id - The id of the activity, must be only digits
  */
-activity.route('/create')
-  .post((req, res) => {
+activity.route('/:id([a-f0-9]+)')
+  .get(async(req, res) => {
+	let getobj = await Activity.findById(req.params.id).exec();
+	
     const statusCode = 200;
-	//res.redirect('/activity/edit/#');
-    res.status(statusCode).json({
-      id: 666,
-      status: statusCode,
-    });
+	const shortened = Boolean(req.query.shortened);
+	
+    res.status(statusCode).send(getobj);
   });
 
 module.exports = activity;
