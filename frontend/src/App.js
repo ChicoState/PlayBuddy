5577f2236ec8b9b749ed2fb6d0d4a654ed783431
Mansi Dogra <30785062+mansidogra1@users.noreply.github.com>
2020-12-02 15:13:55 -0800

    Update Apps.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 import Main from './Main';
 import Header from './Header';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <>
+        <Header />
+        <Main />
+      </>
+    </MuiPickersUtilsProvider>
   );
 }
 
